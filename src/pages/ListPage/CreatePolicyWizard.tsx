9beeffdd3d9e9b6794c7feeb1ddcae5b6eafc1d5
Josejulio Martínez <jmartine@redhat.com>
2020-02-19 06:08:03 -0600

    CPOL-73 Allows to edit policies (#47)

--- src/pages/ListPage/CreatePolicyWizard.tsx
+++ src/pages/ListPage/CreatePolicyWizard.tsx
@@ -1,36 +1,56 @@
 import * as React from 'react';
-import { PolicyWizard } from '../../components/Policy/PolicyWizard';
-import { useCreatePolicyMutation, useVerifyPolicyMutation } from '../../services/Api';
+import { PolicyWizard, SavingMode } from '../../components/Policy/PolicyWizard';
+import { useSavePolicyMutation, useVerifyPolicyMutation } from '../../services/Api';
 import * as HttpStatus from 'http-status-codes';
 import { addSuccessNotification } from '../../utils/AlertUtils';
 import { CreatePolicyResponse, VerifyPolicyResponse } from '../../components/Policy/PolicyWizardTypes';
 import { PolicyWithOptionalId } from '../../types/Policy/Policy';
 
-interface CreatePolicyWizardProps {
+type CreatePolicyWizardBase = {
     close: (policyCreated: boolean) => void;
-    isOpen: boolean;
-}
+    initialValue?: PolicyWithOptionalId;
+    savingMode: SavingMode;
+};
+
+type CreatePolicyWizardIsOpen = {
+    isOpen: true;
+} & CreatePolicyWizardBase;
+
+type CreatePolicyWizardIsClose = {
+    isOpen: false;
+} & Partial<CreatePolicyWizardBase>;
+
+type CreatePolicyWizardProps = CreatePolicyWizardIsClose | CreatePolicyWizardIsOpen;
 
 export const CreatePolicyWizard: React.FunctionComponent<CreatePolicyWizardProps> = (props) => {
-    const saveMutation = useCreatePolicyMutation();
+    const saveMutation = useSavePolicyMutation();
     const verifyMutation = useVerifyPolicyMutation();
+
+    if (props.savingMode === SavingMode.UPDATE && props.initialValue?.id === undefined) {
+        throw new Error('Invalid SavingMode UPDATE for initialValue. initialValue must provide a Policy with an id');
+    }
 
     const onSave = (policy: PolicyWithOptionalId): Promise<CreatePolicyResponse> => {
         return saveMutation.mutate(policy).then((res) => {
             switch (res.status) {
                 case HttpStatus.CREATED:
-                    addSuccessNotification('Created', `Policy "${policy.name}" created`);
-                    props.close(true);
+                case HttpStatus.OK:
+                    if (policy.id === undefined) {
+                        addSuccessNotification('Created', `Policy "${policy.name}" created`);
+                    } else {
+                        addSuccessNotification('Saved', `Policy "${policy.name}" has been updated`);
+                    }
+
+                    props.close && props.close(true);
                     return {
                         created: true
                     };
-                case HttpStatus.BAD_REQUEST:
-                case HttpStatus.CONFLICT:
-                case HttpStatus.INTERNAL_SERVER_ERROR:
                 default:
                     return {
                         created: false,
-                        error: res.payload?.msg || `Unknown Error when trying to create the policy: (Code ${res.status})`
+                        error: res.payload?.msg || `Unknown Error when trying to ${
+                            props.savingMode === SavingMode.CREATE ? 'create' : 'update'
+                        } the policy: (Code ${res.status})`
                     };
             }
         });
@@ -61,10 +81,11 @@ export const CreatePolicyWizard: React.FunctionComponent<CreatePolicyWizardProps
         <>
             { props.isOpen &&
             <PolicyWizard
-                initialValue={ { } }
+                initialValue={ props.initialValue || { } }
                 onClose={ () => { props.close(false); } }
                 onSave={ onSave }
                 onVerify={ onVerify }
+                savingMode={ props.savingMode }
                 isLoading={ isLoading }
             /> }
         </>
