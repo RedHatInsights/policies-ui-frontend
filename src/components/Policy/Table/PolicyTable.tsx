80158d62db6a7b129c919533e42c53e1f15fde2a
Simeon Pinder <spinder@redhat.com>
2020-02-26 19:26:35 -0500

    CPOL-101 Change default messaging for some table entries.

--- src/components/Policy/Table/PolicyTable.tsx
+++ src/components/Policy/Table/PolicyTable.tsx
@@ -19,6 +19,7 @@ import { CheckCircleIcon, OffIcon } from '@patternfly/react-icons';
 import { Direction, Sort } from '../../../types/Page';
 import { ExpandedContent } from './ExpandedContent';
 import { IconType } from '@patternfly/react-icons/dist/js/createIcon';
+import { Messages } from '../../../properties/Messages';
 
 interface PolicyTableProps {
     actions?: IActions;
@@ -92,8 +93,8 @@ const policiesToRows = (policies: PolicyRow[] | undefined): IRow[] => {
                     <>
                         <ExpandedContent
                             key={ policy.id + '-content' }
-                            description={ policy.description }
-                            conditions={ policy.conditions }
+                            description={ policy.description ? policy.description : Messages.tableDoubleDash + ' ' + Messages.tableNoDescription }
+                            conditions={ policy.conditions ? policy.conditions : Messages.tableNoConditions }
                             actions={ policy.actions }
                             created={ new Date(2020, 1, 19) }
                             updated={ policy.mtime }
@@ -210,7 +211,7 @@ export const PolicyTable: React.FunctionComponent<PolicyTableProps> = (props) =>
 
     return (
         <Table
-            aria-label="Custom policies"
+            aria-label={ Messages.tableTitle }
             cells={ columns }
             rows={ rows }
             actionResolver={ actionsResolver }
--- src/properties/Messages.ts
+++ src/properties/Messages.ts
@@ -2,6 +2,12 @@
 //Capture some strings we reuse. Possibly use in i18n later?
 
 export class Messages {
+    public static readonly tableDoubleDash: string = '--';
+    public static readonly tableNeverRun: string = 'Never run' ;
+    public static readonly tableNoActions: string = 'No actions' ;
+    public static readonly tableNoConditions: string = 'No conditions' ;
+    public static readonly tableNoDescription: string = 'No description' ;
+    public static readonly tableTitle: string = 'Custom Policies';
     public static readonly wizardActions: string = 'Actions' ;
     public static readonly wizardConditions: string = 'Conditions' ;
     public static readonly wizardCreatePolicy: string ='Create Custom Policy';
