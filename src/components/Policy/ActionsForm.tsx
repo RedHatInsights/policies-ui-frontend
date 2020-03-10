3d89eac0c342f22f15b9a5f0a1873888b2a95d86
Josejulio Martínez <jmartine@redhat.com>
2020-03-10 10:10:00 -0600

    CPOL-113 UXD changes for "Add actions"   - Changed from button to dropdown-link   - Moved to top

--- src/components/Policy/ActionsForm.tsx
+++ src/components/Policy/ActionsForm.tsx
@@ -2,19 +2,22 @@ import { ArrayHelpers } from 'formik';
 import { DeepPartial } from 'ts-essentials';
 import { Action, ActionType } from '../../types/Policy/Actions';
 import {
-    ActionGroup,
-    Button, capitalize,
+    Button,
     Card,
     CardActions,
     CardBody,
-    CardHead, CardHeader,
-    FormSelectOption
+    CardHead, CardHeader, Form,
+    Title
 } from '@patternfly/react-core';
 import * as React from 'react';
 import { TimesIcon } from '@patternfly/react-icons';
-import { FormSelect } from '../Formik/Patternfly';
 import { ActionForm } from './ActionForm/ActionForm';
-import { getInsights } from '../../utils/Insights';
+import { assertNever } from '../../utils/Assert';
+import { style } from 'typestyle';
+
+const marginTopClassName = style({
+    marginTop: 10
+});
 
 interface ActionsFormProps {
     id: string;
@@ -24,41 +27,42 @@ interface ActionsFormProps {
     isReadOnly?: boolean;
 }
 
+const titleForActionType = (actionType: ActionType) => {
+    switch (actionType) {
+        case ActionType.EMAIL:
+            return 'Send email';
+        case ActionType.WEBHOOK:
+            return 'Send to hook';
+        default:
+            assertNever(actionType);
+    }
+};
+
 export const ActionsForm: React.FunctionComponent<ActionsFormProps> = (props) => {
     return (
-        <>
+        <Form>
             { props.actions?.map((action, index) => (
                 <React.Fragment key={ index }>
-                    <Card>
+                    <Card className={ marginTopClassName }>
                         <CardHead>
                             <CardActions>
                                 <Button variant="plain" aria-label="Action" onClick={ props.arrayHelpers.handleRemove(index) }>
                                     <TimesIcon/>
                                 </Button>
                             </CardActions>
-                            <CardHeader>Action #{ index + 1 }</CardHeader>
+                            <CardHeader>
+                                <Title size="sm">{ action?.type ? titleForActionType(action.type) : '' }</Title>
+                            </CardHeader>
                         </CardHead>
                         { /* Adding this pf-c-form class is a hack (i think).
                         It looks like We should not use Card inside the Form as it breaks the layout.
                         */ }
                         <CardBody className="pf-c-form">
-                            <FormSelect isDisabled={ props.isReadOnly } id={ `actions.${index}.type` } name={ `actions.${index}.type` } label="Type">
-                                <FormSelectOption value="" label="Select an Action type"/>
-                                { Object.values(ActionType)
-                                .filter(async actionType => (await getInsights()).chrome.isBeta() || actionType !== ActionType.WEBHOOK)
-                                .map(type => <FormSelectOption
-                                    key={ type }
-                                    label={ capitalize(type) }
-                                    value={ type }/>)}
-                            </FormSelect>
                             <ActionForm isReadOnly={ props.isReadOnly } action={ action } prefix={ `actions.${index}` }/>
                         </CardBody>
                     </Card>
                 </React.Fragment>
             )) }
-            <ActionGroup>
-                { !props.isReadOnly && <Button variant="primary" onClick={ props.arrayHelpers.handlePush({}) }>+ Add action</Button> }
-            </ActionGroup>
-        </>
+        </Form>
     );
 };
--- src/components/Policy/WizardSteps/ActionsStep.tsx
+++ src/components/Policy/WizardSteps/ActionsStep.tsx
@@ -1,12 +1,51 @@
 import * as React from 'react';
-import { Title } from '@patternfly/react-core';
+import { Button, ButtonVariant, capitalize, Dropdown, DropdownItem, Title } from '@patternfly/react-core';
 
 import { PartialPolicy, WizardStepExtended } from '../PolicyWizardTypes';
 import { PolicyFormActions } from '../../../schemas/CreatePolicy/PolicySchema';
 import { FieldArray, FieldArrayRenderProps, useFormikContext } from 'formik';
 import { ActionsForm } from '../ActionsForm';
 import { Messages } from '../../../properties/Messages';
-import { Form } from '../../Formik/Patternfly/Form';
+import { ActionType } from '../../../types/Policy/Actions';
+import { getInsights } from '../../../utils/Insights';
+import { Toggle } from '@patternfly/react-core/dist/js/components/Dropdown/Toggle';
+import { AngleDownIcon } from '@patternfly/react-icons';
+
+interface AddTriggersDropdownProps {
+    addType: (type: ActionType) => void;
+    isTypeEnabled: (type: ActionType) => boolean;
+}
+
+const AddTriggersDropdown: React.FunctionComponent<AddTriggersDropdownProps> = (props) => {
+    const [ isOpen, setOpen ] = React.useState<boolean>(false);
+
+    const typeSelected = type => {
+        props.addType(type);
+        setOpen(false);
+    };
+
+    const items = Object.values(ActionType)
+    .filter(async actionType => (await getInsights()).chrome.isBeta() || actionType !== ActionType.WEBHOOK)
+    .map(type =>
+        <DropdownItem
+            key={ type }
+            onClick={ () => typeSelected(type) }
+            isDisabled={ !props.isTypeEnabled(type) }
+        >
+            { capitalize(type) }
+        </DropdownItem>);
+
+    return (
+        <Dropdown
+            isOpen={ isOpen }
+            dropdownItems={ items }
+            isPlain
+            toggle={ <Toggle isPlain onToggle={ open => setOpen(open) } id="add-action-toggle">
+                <Button component="a" variant={ ButtonVariant.link } isInline> Add trigger actions <AngleDownIcon/> </Button>
+            </Toggle> }
+        />
+    );
+};
 
 const ActionsStep = () => {
 
@@ -21,14 +60,31 @@ const ActionsStep = () => {
     }, [ validateForm, actionsLength ]);
 
     return (
-        <Form>
+        <>
             <Title headingLevel="h4" size="xl">{ Messages.wizards.policy.actions.title }</Title>
             <FieldArray name="actions">
                 { (helpers: FieldArrayRenderProps) => {
-                    return <ActionsForm id="actions" name="actions" actions={ helpers.form.values.actions } arrayHelpers={ helpers }/>;
+                    const isTypeEnabled = (actionType: ActionType) => {
+                        const actionsOfType = helpers.form.values.actions?.filter(action => action.type === actionType) || [];
+                        return actionsOfType.length === 0;
+                    };
+
+                    const addType = (actionType: ActionType) => {
+                        helpers.push({ type: actionType });
+                    };
+
+                    return (
+                        <>
+                            <AddTriggersDropdown
+                                isTypeEnabled={ isTypeEnabled }
+                                addType={ addType }
+                            />
+                            <ActionsForm id="actions" name="actions" actions={ helpers.form.values.actions } arrayHelpers={ helpers }/>
+                        </>
+                    );
                 } }
             </FieldArray>
-        </Form>
+        </>
     );
 };
 
