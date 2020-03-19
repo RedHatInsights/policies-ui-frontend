76ebfe2ca78116abcaef5c0489cf14c6aa7076e9
Josejulio Martínez <jmartine@redhat.com>
2020-03-19 11:05:58 -0600

    CPOL-100 UXD changes on Wizard:Reviews (#71)

--- src/components/Policy/WizardSteps/ReviewStep.tsx
+++ src/components/Policy/WizardSteps/ReviewStep.tsx
@@ -1,38 +1,57 @@
 import * as React from 'react';
-import { FieldArray, FieldArrayRenderProps } from 'formik';
+import { useFormikContext } from 'formik';
 
-import { WizardContext, WizardStepExtended } from '../PolicyWizardTypes';
-import { FormTextInput, Switch } from '../../Formik/Patternfly';
+import { PartialPolicy, WizardContext, WizardStepExtended } from '../PolicyWizardTypes';
+import { Switch, FormText } from '../../Formik/Patternfly';
 import { Split, SplitItem, Title } from '@patternfly/react-core';
-import { ActionsForm } from '../ActionsForm';
 import { PolicyFormSchema } from '../../../schemas/CreatePolicy/PolicySchema';
 import { useContext } from 'react';
-import { GlobalDangerColor100 } from '../../../utils/PFColors';
+import { GlobalDangerColor100, GlobalBackgroundColorLight300 } from '../../../utils/PFColors';
 import { ExclamationCircleIcon } from '@patternfly/react-icons';
 import { style } from 'typestyle';
 import { Messages } from '../../../properties/Messages';
 import { Form } from '../../Formik/Patternfly/Form';
+import { Conditions } from '../Table/ExpandedContent/Conditions';
+import { Actions } from '../Table/ExpandedContent/Actions';
+import { Action } from '../../../types/Policy/Actions';
+import { padding } from 'csstips';
 
 const exclamationClassName = style({
     marginRight: 5
 });
 
+const conditionsAndActionsBlockClassName = style({
+    backgroundColor: GlobalBackgroundColorLight300,
+    ...padding(18, 14),
+    margin: 5
+});
+
 const ReviewStep: React.FunctionComponent = () => {
     const context = useContext(WizardContext);
+    const { values } = useFormikContext<PartialPolicy>();
 
     return (
         <>
             <Form>
-                <Title headingLevel="h4" size="xl">{ Messages.wizards.policy.review.title }</Title>
-                <Switch isDisabled={ context.isLoading } type="checkbox" id="isEnabled" name="isEnabled" label="Activate Policy?"/>
-                <FormTextInput isReadOnly label="Name" type="text" name="name" id="name"/>
-                <FormTextInput isReadOnly label="Description" type="text" id="description" name="description"/>
-                <FormTextInput isReadOnly label="Condition text" type="text" id="conditions" name="conditions"/>
-                <FieldArray name="actions">
-                    { (helpers: FieldArrayRenderProps) => {
-                        return <ActionsForm id="actions" name="actions" isReadOnly actions={ helpers.form.values.actions } arrayHelpers={ helpers }/>;
-                    } }
-                </FieldArray>
+                <Title size="xl">{ Messages.wizards.policy.review.title }</Title>
+                <Switch
+                    isDisabled={ context.isLoading }
+                    type="checkbox"
+                    id="isEnabled"
+                    name="isEnabled"
+                    labelOn={ Messages.wizards.policy.review.policyIsActive }
+                    labelOff={ Messages.wizards.policy.review.policyIsNotActive }
+                    label={ Messages.wizards.policy.review.activateThisPolicy }
+                />
+                <Title size="md">{ Messages.wizards.policy.review.policy.details }</Title>
+                <FormText label={ Messages.wizards.policy.review.policy.name } name="name" id="name"/>
+                <FormText label={ Messages.wizards.policy.review.policy.description } name="description" id="description"/>
+                <div className={ conditionsAndActionsBlockClassName }>
+                    <Conditions conditions={ values.conditions }/>
+                </div>
+                <div className={ conditionsAndActionsBlockClassName }>
+                    <Actions actions={ (values.actions || []) as Action[] } />
+                </div>
                 { context.createResponse.error && (
                     <Split>
                         <SplitItem>
