25298d7988a56ac2a25a7cbe8796f61a43dd7348
Josejulio Martínez <jmartine@redhat.com>
2020-04-03 15:11:40 -0600

    Fix lint issues (unused imports)

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,13 +1,9 @@
 import Config from '../config/Config';
-import { PagedServerPolicyResponse, Policy } from '../types/Policy/Policy';
-import { useMutation, useQuery, useBulkMutation } from 'react-fetching-library';
-import { Page } from '../types/Page';
-import { toPolicies, toServerPolicy } from '../utils/PolicyAdapter';
-import { UsePaginatedQueryResponse, useTransformQueryResponse } from '../utils/ApiUtils';
+import { Policy } from '../types/Policy/Policy';
+import { useBulkMutation, useMutation, useQuery } from 'react-fetching-library';
+import { toServerPolicy } from '../utils/PolicyAdapter';
 import { DeepPartial } from 'ts-essentials';
-import { useNewPaginatedQuery } from '../hooks/usePaginated';
 import { actionBuilder } from './Api/ActionBuilder';
-import { paginatedActionBuilder } from './Api/PaginatedActionBuilder';
 
 const urls = Config.apis.urls;
 
