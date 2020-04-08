7a57ec78020a51b04a0b1c73fd893cf69751cc9b
Josejulio Martínez <jmartine@redhat.com>
2020-04-08 11:21:50 -0500

    POL-192 Change style for trigger action boxes (#99)

---
+++ src/utils/Spacer.tsx
@@ -0,0 +1,12 @@
+const RawSpacer = {
+    XS: 4,
+    SM: 8,
+    MD: 16,
+    LG: 24,
+    XL: 32,
+    XL_2: 48,
+    XL_3: 64,
+    FORM_ELEMENT: 6
+};
+
+export const Spacer: Readonly<typeof RawSpacer> = RawSpacer;
