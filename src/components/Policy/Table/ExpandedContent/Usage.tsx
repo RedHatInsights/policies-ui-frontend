5e44f3ae2a101c14fb24f53546ab0165e342a5c2
Simeon Pinder <spinder@redhat.com>
2020-04-17 22:07:33 -0400

    i)Change from Accordian to Expandable ii)rename toggle function.

--- src/components/Policy/Table/ExpandedContent/Usage.tsx
+++ src/components/Policy/Table/ExpandedContent/Usage.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-import { StackItem, TextContent, Text, TextVariants } from '@patternfly/react-core';
+import { StackItem, TextContent, Text, TextVariants, Expandable } from '@patternfly/react-core';
 import { Messages }  from '../../../../properties/Messages';
-
-const accordianString = 'pf-c-accordion__expanded-content';
-const toggleString = 'pf-c-accordion__toggle';
 
 interface UsageProps {
     showHint?: boolean;
@@ -35,31 +32,16 @@ class TextItemExtension extends React.Component {
 
 const Usage: React.FunctionComponent<UsageProps> = (_props) => {
     const [ usageState, setUsageState ] = React.useState<UsageProps>({ ..._props, showHint: false });
-    const change = () =>{
+    const toggle = () =>{
         setUsageState({ ..._props, showHint: !usageState.showHint });
     };
 
     return (
         <>
             <StackItem>
-                <div className="pf-c-accordion">
-                    <h3>
-                        <button className={ usageState.showHint ? (toggleString + ' pf-m-expanded') : (toggleString + '') }
-                            aria-expanded={ usageState.showHint } onClick={ () => change() }>
-                            <span className="pf-c-accordion__toggle-text">{ usageState.hintTitle }</span>
-                            <i className="fas fa-angle-right pf-c-accordion__toggle-icon" aria-hidden='true'></i>
-                        </button>
-                    </h3>
-                    {usageState.showHint ? (
-                        <div className={ usageState.showHint ? (accordianString + ' pf-m-expanded')  : (accordianString) }>
-                            <div className="pf-c-accordion__expanded-content-body"><TextItemExtension/></div>
-                        </div>
-                    ) : (
-                        <div className={ usageState.showHint ? (accordianString + ' pf-m-expanded')  : (accordianString) } hidden>
-                            <div className="pf-c-accordion__expanded-content-body"><TextItemExtension/></div>
-                        </div>
-                    )}
-                </div>
+                <Expandable toggleText={ usageState.hintTitle } onToggle={ toggle }>
+                    <TextItemExtension/>
+                </Expandable>
             </StackItem>
         </>
     );
