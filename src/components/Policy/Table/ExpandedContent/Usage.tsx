75747fd691a69fce4482b43a66298142243f1a09
Simeon Pinder <spinder@redhat.com>
2020-04-20 13:43:09 -0400

    A few more tweaks to the sizing.

--- src/components/Policy/Table/ExpandedContent/Usage.tsx
+++ src/components/Policy/Table/ExpandedContent/Usage.tsx
@@ -8,6 +8,9 @@ const codeClass = style({
     fontSize: '14px',
     fontStyle: 'default'
 });
+const linkMarginClassName = style({
+    marginBottom: 5
+});
 
 // build the text as we expect it to look but this is manual and not automatic
 // We'll need to revisit this later.
@@ -16,15 +19,15 @@ class TextItemExtension extends React.Component {
         return (
             <TextContent>
                 <Text component={ TextVariants.p }>{ Messages.wizards.policy.hints.hintParagraph1 }</Text>
-                <Text component={ TextVariants.a } href="#">{ Messages.wizards.policy.hints.hintLinkTitle }</Text>
-                <Text component={ TextVariants.h5 } className="pf-m-lg">{ Messages.wizards.policy.hints.hintSyntaxExamplesSection }</Text>
-                <Text component={ TextVariants.h6 }>{ Messages.wizards.policy.hints.hintQuestion1 }</Text>
+                <Text component={ TextVariants.a } className={ linkMarginClassName } href="#">{ Messages.wizards.policy.hints.hintLinkTitle }</Text>
+                <Text component={ TextVariants.h5 } className="lg">{ Messages.wizards.policy.hints.hintSyntaxExamplesSection }</Text>
+                <Text >{ Messages.wizards.policy.hints.hintQuestion1 }</Text>
                 <Text component={ TextVariants.p } className={ codeClass }><i>{ Messages.wizards.policy.hints.hintQuestion1Ans }</i></Text>
-                <Text component={ TextVariants.h6 }>{ Messages.wizards.policy.hints.hintQuestion2 }</Text>
+                <Text >{ Messages.wizards.policy.hints.hintQuestion2 }</Text>
                 <Text component={ TextVariants.p } className={ codeClass }><i>{ Messages.wizards.policy.hints.hintQuestion2Ans }</i></Text>
-                <Text component={ TextVariants.h6 }>{ Messages.wizards.policy.hints.hintQuestion3 }</Text>
+                <Text >{ Messages.wizards.policy.hints.hintQuestion3 }</Text>
                 <Text component={ TextVariants.p } className={ codeClass }><i>{ Messages.wizards.policy.hints.hintQuestion3Ans }</i></Text>
-                <Text component={ TextVariants.h6 }>{ Messages.wizards.policy.hints.hintQuestion4 }</Text>
+                <Text >{ Messages.wizards.policy.hints.hintQuestion4 }</Text>
                 <Text component={ TextVariants.p } className={ codeClass }><i>{ Messages.wizards.policy.hints.hintQuestion4Ans }</i></Text>
             </TextContent>
         );
