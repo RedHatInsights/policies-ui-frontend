69c25cfcf61a648fb473a815f26aa7ce94744900
Simeon Pinder <spinder@redhat.com>
2020-05-27 18:29:36 -0400

    Add a few more optional display components.

--- src/components/Pf/components/form/PfForm.tsx
+++ src/components/Pf/components/form/PfForm.tsx
@@ -1,28 +1,54 @@
 import * as React from 'react';
 import { hyphenate } from '../../../../utils/ComponentUtils';
-import { Form, FormGroup } from '@patternfly/react-core';
+import { Form, FormGroup, TextInput } from '@patternfly/react-core';
 
-const formName = 'simple-form';//label name
-
-// Uses the following component to create i)Form labels with optional
-// ii)required iii)popover iv)HelpText, etc..
-export class VAlignForm extends React.Component<{
+interface VAlignFormProps {
     title?: string;
     identification?: string;
     showAsRequired?: boolean;
     showPopover?: boolean;
+    showHelpText?: boolean;
+    showInputField?: boolean;
+    showInputFieldAsRequired?: boolean;
     popoverMessage?: string;
     helpText?: string;
     children?: React.ReactNode;
-}> {
+}
+
+const formName = 'simple-form';//label name
+const formInputName = 'simple-form-input';//label name
+
+// Uses the following component to create i)Form labels with optional
+// ii)required iii)popover iv)HelpText, etc..
+export class VAlignForm extends React.Component< VAlignFormProps> {
+    constructor(props) {
+        super(props);
+    }
+    handleTextInputChange1() {
+        //override with implementation logic
+    };
+
     render() {
         return (
             <Form>
-                { !this.props.showAsRequired && <FormGroup label={ this.props.title }
+                { !this.props.showInputField && <FormGroup label={ this.props.title }
                     fieldId={ hyphenate(formName, (this.props.identification ? this.props.identification : '')) }
+                    helperText={ this.props.showHelpText && this.props.helpText } isRequired={ this.props.showAsRequired }
                 />}
-                { this.props.showAsRequired && <FormGroup label={ this.props.title } isRequired
-                    fieldId={ hyphenate(formName, (this.props.identification ? this.props.identification : '')) }/>
+                { this.props.showInputField &&
+                <FormGroup label={ this.props.title }
+                    fieldId={ hyphenate(formName, (this.props.identification ? this.props.identification : '')) }
+                    helperText={ this.props.showHelpText && this.props.helpText } isRequired={ this.props.showAsRequired }>
+                    <TextInput
+                        isRequired={ this.props.showInputFieldAsRequired }
+                        type="text"
+                        id={ hyphenate(formInputName, (this.props.identification ? this.props.identification : '')) }
+                        name={ hyphenate(formInputName, (this.props.identification ? this.props.identification : '')) }
+                        aria-describedby="simple-form-name-helper"
+                        value="(change me)"
+                        onChange={ this.handleTextInputChange1 }
+                    />
+                </FormGroup>
                 }
             </Form>
         );
