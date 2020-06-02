de791ab87b9144dbeb6d7b4e88ea01ecc4c2c65d
Josejulio Martínez <jmartine@redhat.com>
2020-06-02 14:20:24 -0500

    PR feedback: Katie  - Remove empty state cubes icon

--- src/components/Trigger/Table/EmptyState.tsx
+++ src/components/Trigger/Table/EmptyState.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import { CubesIcon } from '@patternfly/react-icons';
 import { EmptyStateSection } from '../../Policy/EmptyState/Section';
 import { Messages } from '../../../properties/Messages';
 
 export const TriggerTableEmptyState: React.FunctionComponent = () => {
     return <EmptyStateSection
-        icon={ CubesIcon }
         title={ Messages.tables.trigger.emptyState.notFound.title }
         content={ Messages.tables.trigger.emptyState.notFound.content }
     />;
--- src/pages/PolicyDetail/TriggerEmptyState.tsx
+++ src/pages/PolicyDetail/TriggerEmptyState.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import { CubesIcon } from '@patternfly/react-icons';
 import { Messages } from '../../properties/Messages';
 import { EmptyStateSection } from '../../components/Policy/EmptyState/Section';
 
 export const PolicyDetailTriggerEmptyState: React.FunctionComponent = () => {
     return <EmptyStateSection
-        icon={ CubesIcon }
         title={ Messages.pages.policyDetail.triggerEmptyState.title }
         content={ Messages.pages.policyDetail.triggerEmptyState.text }
     />;
