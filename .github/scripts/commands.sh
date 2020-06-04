467fc86cdd51c6144223ae8f935eba544b3356d0
Josejulio Martínez <jmartine@redhat.com>
2020-06-04 10:59:11 -0500

    POL-285 Relax expression to turn script's src also

--- .github/scripts/commands.sh
+++ .github/scripts/commands.sh
@@ -3,9 +3,9 @@
 TARGET="${TARGET:-dist/index.html}"
 
 function ensure_beta {
-  sed -i 's/include src=\"\/apps/include src=\"\/beta\/apps/g' "${TARGET}"
+  sed -i 's/src=\"\/apps/src=\"\/beta\/apps/g' "${TARGET}"
 }
 
 function ensure_stable {
-  sed -i 's/include src=\"\/beta\/apps/include src=\"\/apps/g' "${TARGET}"
+  sed -i 's/src=\"\/beta\/apps/src=\"\/apps/g' "${TARGET}"
 }
