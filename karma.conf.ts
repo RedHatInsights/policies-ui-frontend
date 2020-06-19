42094edeac5757e03bb666feebf9be42a471e15a
Josejulio Martínez <jmartine@redhat.com>
2020-06-19 17:36:06 -0500

    Use headless

--- karma.conf.ts
+++ karma.conf.ts
@@ -75,11 +75,11 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: [ 'Chrome', 'Firefox' ],
+        browsers: [ 'ChromeHeadless', 'FirefoxHeadless' ],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+        singleRun: true,
 
         client: {
 
