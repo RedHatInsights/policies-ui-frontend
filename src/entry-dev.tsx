7a6d7c0a9177d0fb9765ec13faf7517cf674837a
Josejulio Martínez <jmartine@redhat.com>
2020-07-17 14:02:35 -0500

    Fixes  - Patch insights build config to allow tsx entry points  - Update insights-common-typescript lib

--- src/entry-dev.tsx
+++ src/entry-dev.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { initStore, createFetchingClient, getBaseName, getStore } from '@redhat-cloud-services/insights-common-typescript';
+import {
+    initStore,
+    createFetchingClient,
+    getBaseName,
+    getStore,
+    getInsights
+} from '@redhat-cloud-services/insights-common-typescript';
 import App from './app/App';
+import logger from 'redux-logger';
 import { ClientContextProvider } from 'react-fetching-library';
 
-const client = createFetchingClient();
-initStore();
+const client = createFetchingClient(getInsights);
+initStore(logger);
 
 ReactDOM.render(
     <Provider store={ getStore() }>
         <Router basename={ getBaseName(window.location.pathname) }>
             <ClientContextProvider client={ client }>
-                <App />
+                <App/>
             </ClientContextProvider>
         </Router>
     </Provider>,
