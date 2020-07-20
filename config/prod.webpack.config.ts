cdfb7dd92efdfd31ad03289588294d91e9e010fe
Josejulio Martínez <jmartine@redhat.com>
2020-07-20 16:29:22 -0500

    Remove config patch as this is fixed in frontend-components-config

--- config/prod.webpack.config.ts
+++ config/prod.webpack.config.ts
@@ -1,12 +1,9 @@
 import { resolve } from 'path';
 import config from '@redhat-cloud-services/frontend-components-config';
-import { patchConfig } from './patch';
 
 const { config: webpackConfig, plugins } = config({
     rootFolder: resolve(__dirname, '../')
 });
-
-patchConfig(webpackConfig);
 
 module.exports = {
     ...webpackConfig,
