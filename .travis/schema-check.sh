f3b6c943a99265b0f83ebfab1bb516bb3ab21ba0
Josejulio Martínez <jmartine@redhat.com>
2020-08-10 16:54:14 -0500

    Trying to speed up runs

--- .travis/schema-check.sh
+++ .travis/schema-check.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-yarn schema:generate -i https://cloud.redhat.com/api/policies/v1.0/openapi.json
-[[ -z $(git status --porcelain) ]] ||
-(
-  git status &&
-  git diff &&
-  echo "----------------------------------------------------------------" &&
-  echo "  openapi.json types or actions are out of sync aborting build. " &&
-  echo "----------------------------------------------------------------" &&
-  false
-)
+if [ "${TRAVIS_BRANCH}" = "prod" ] || [ "${TRAVIS_BRANCH}" = "prod-stable" ]
+then
+    yarn schema:generate -i https://cloud.redhat.com/api/policies/v1.0/openapi.json
+    [[ -z $(git status --porcelain) ]] ||
+    (
+      git status &&
+      git diff &&
+      echo "----------------------------------------------------------------" &&
+      echo "  openapi.json types or actions are out of sync aborting build. " &&
+      echo "----------------------------------------------------------------" &&
+      false
+    )
+fi
