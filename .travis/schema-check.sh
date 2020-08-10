42747084dbfa42e8198fed120841fda520a96bb4
Josejulio Martínez <jmartine@redhat.com>
2020-08-10 16:27:51 -0500

    Split travis job in multiple stages to make it easier to know where it failed

--- .travis/schema-check.sh
+++ .travis/schema-check.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" = "prod" ]
-then
-    yarn schema:generate -i https://cloud.redhat.com/api/policies/v1.0/openapi.json
-    [[ -z $(git status --porcelain) ]] ||
-    (
-      git status &&
-      git diff &&
-      echo "----------------------------------------------------------------" &&
-      echo "  openapi.json types or actions are out of sync aborting build. " &&
-      echo "----------------------------------------------------------------" &&
-      false
-    )
-fi
+yarn schema:generate -i https://cloud.redhat.com/api/policies/v1.0/openapi.json
+[[ -z $(git status --porcelain) ]] ||
+(
+  git status &&
+  git diff &&
+  echo "----------------------------------------------------------------" &&
+  echo "  openapi.json types or actions are out of sync aborting build. " &&
+  echo "----------------------------------------------------------------" &&
+  false
+)
