f3a956974b98e57ff1ee3c739f7dfcc16ad2b487
Josejulio Martínez <jmartine@redhat.com>
2021-04-19 10:37:39 -0500

    POL-489 Update wording for "Enable email alerts" warning

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -20,7 +20,7 @@ const Config = {
     },
     pages: {
         emailPreferences: () => localUrl('/user-preferences/notifications/insights', getInsights().chrome.isBeta()),
-        notifications: () => localUrl('/settings/notifications', getInsights().chrome.isBeta()),
+        notifications: () => localUrl('/settings/notifications/rhel', getInsights().chrome.isBeta()),
         // eslint-disable-next-line max-len
         factsDocumentation: 'https://access.redhat.com/documentation/en-us/red_hat_insights/2020-04/html/monitoring_and_reacting_to_configuration_changes_using_policies/appendix-policies#facts-and-functions'
     }
