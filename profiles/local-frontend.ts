092ebc73c8da028ea5ee061acfee927f1f53856d
Josejulio Martínez <jmartine@redhat.com>
2021-06-02 17:45:57 -0500

    Dependency upgrades (#251)
    * Update @redhat-cloud-services* deps
    
    * Update dev setup to use https
    
    * Update node version to 14 (LTS)
    
    * Fixed karma tests
     - Updated karma
     - Removed cacheGroups from config

--- profiles/local-frontend.ts
+++ profiles/local-frontend.ts
@@ -8,11 +8,11 @@ const APP_ID = 'policies';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
-routes[`/${SECTION}/${APP_ID}`]      = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/apps/${APP_ID}`]            = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/beta/${SECTION}/${APP_ID}`] = { host: `http://localhost:${FRONTEND_PORT}` };
-routes[`/beta/apps/${APP_ID}`]       = { host: `http://localhost:${FRONTEND_PORT}` };
+routes[`/${SECTION}/${APP_ID}`]      = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/apps/${APP_ID}`]            = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/${SECTION}/${APP_ID}`] = { host: `https://localhost:${FRONTEND_PORT}` };
+routes[`/beta/apps/${APP_ID}`]       = { host: `https://localhost:${FRONTEND_PORT}` };
 
 module.exports = {
     routes,
