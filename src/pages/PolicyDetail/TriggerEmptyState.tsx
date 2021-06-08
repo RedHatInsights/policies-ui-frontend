c3224e313fc6619e274063430381d6c899c75bbf
Josejulio Martínez <jmartine@redhat.com>
2021-06-08 17:32:08 -0500

    POL-345 Add retention info for triggers history

--- src/pages/PolicyDetail/TriggerEmptyState.tsx
+++ src/pages/PolicyDetail/TriggerEmptyState.tsx
@@ -1,3 +1,5 @@
+import { Text, TextContent } from '@patternfly/react-core';
+import { global_palette_black_600 } from '@patternfly/react-tokens';
 import * as React from 'react';
 
 import { EmptyStateSection } from '../../components/Policy/EmptyState/Section';
@@ -6,6 +8,11 @@ import { Messages } from '../../properties/Messages';
 export const PolicyDetailTriggerEmptyState: React.FunctionComponent = () => {
     return <EmptyStateSection
         title={ Messages.pages.policyDetail.triggerEmptyState.title }
-        content={ Messages.pages.policyDetail.triggerEmptyState.text }
+        content={ <TextContent>
+            <Text style={ {
+                color: global_palette_black_600.value
+            } } >{ Messages.pages.policyDetail.triggerEmptyState.text }</Text>
+            <Text component="small">{ Messages.pages.policyDetail.triggerEmptyState.sub }</Text>
+        </TextContent> }
     />;
 };
