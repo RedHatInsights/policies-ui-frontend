bc264e94c512d120bc5cfbffcd9f03a1c1263ec7
Josejulio Martínez <jmartine@redhat.com>
2021-09-30 15:40:26 -0500

    POL-544 Navigate to policies list when clicking on "Policies" menu

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,7 @@
+import { getBaseName, getInsights } from '@redhat-cloud-services/insights-common-typescript';
 import * as React from 'react';
-import { Redirect, Route, RouteProps, Switch } from 'react-router';
+import { useEffect } from 'react';
+import { matchPath, Redirect, Route, RouteProps, Switch, useHistory } from 'react-router';
 
 import { ErrorPage } from './pages/Error/Page';
 import ListPage from './pages/ListPage/ListPage';
@@ -36,7 +38,44 @@ const InsightsRoute: React.FunctionComponent<InsightsRouteProps> = (props: Insig
     );
 };
 
+const relativePath = (base: string, pathname: string) => {
+    const relative = pathname.substr(base.length).trim();
+    if (relative.length === 0) {
+        return linkTo.listPage();
+    }
+
+    return relative;
+};
+
 export const Routes: React.FunctionComponent<unknown> = () => {
+    const insights = getInsights();
+    const history = useHistory();
+
+    useEffect(() => {
+        const on = insights.chrome.on;
+        if (on) {
+            return on('APP_NAVIGATION', event => {
+                const pathname = event.domEvent.href;
+                const base = getBaseName(pathname);
+                const relative = relativePath(base, pathname);
+
+                for (const route of pathRoutes) {
+                    if (matchPath(relative, {
+                        path: route.path,
+                        exact: true
+                    })) {
+                        if (history.location.pathname !== relative) {
+                            history.push(relative);
+                        }
+
+                        break;
+                    }
+                }
+
+            });
+        }
+    }, [ insights.chrome.on, history ]);
+
     return (
         <Switch>
             { pathRoutes.map(pathRoute => (
