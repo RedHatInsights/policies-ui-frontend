55ef9a5740a66db75904b0e50374fcb563ec8306
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-07-06 08:12:35 -0600

    Remove index.html patching (#329)
    - This was used to patch the index for beta/non-beta depending the deployment
       index.html is no longer generated when using the frontend-config 4.6.10+

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -2,11 +2,8 @@
 set -e
 set -x
 
-source ./.github/scripts/commands.sh
-
 if [ "${TRAVIS_BRANCH}" = "master" ]
 then
-    ensure_beta
     for env in ci qa
     do
         echo "PUSHING ${env}-beta"
@@ -14,7 +11,6 @@ then
         .travis/release.sh "${env}-beta"
     done
 
-    ensure_stable
     for env in ci qa
     do
         echo "PUSHING ${env}-stable"
@@ -25,12 +21,10 @@ fi
 
 if [ "${TRAVIS_BRANCH}" = "prod" ]
 then
-    ensure_beta
     echo "PUSHING prod-beta"
     rm -rf ./dist/.git
     .travis/release.sh "prod-beta"
 
-    ensure_stable
     echo "PUSHING prod-stable"
     rm -rf ./dist/.git
     .travis/release.sh "prod-stable"
@@ -39,7 +33,6 @@ fi
 # Pushing to a release branch ({ci|qa|prod}-{beta|stable} if we want to override a specific environment
 if [[ "${TRAVIS_BRANCH}" =~ ^(ci|qa|prod)-(stable|beta)$  ]]
 then
-  # No need to call ensure_beta/stable, insights libs will take care from that if this is a release branch
   echo "PUSHING ${TRAVIS_BRANCH}"
   rm -rf ./dist/.git
   .travis/release.sh "${TRAVIS_BRANCH}"
