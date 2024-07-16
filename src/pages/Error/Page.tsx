384a21f8ee82ed6d38e3c74f298cebd147167fcf
leSamo <soleksak@redhat.com>
2024-07-16 16:01:25 +0200

    Remove `withRouter`

--- src/pages/Error/Page.tsx
+++ src/pages/Error/Page.tsx
@@ -1,13 +1,9 @@
 import { ErrorBoundary } from '@redhat-cloud-services/frontend-components';
 import * as React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 import { Messages } from '../../properties/Messages';
 
-type ErrorPageProps = RouteComponentProps<any>;
-
-export const ErrorPageInternal: React.FunctionComponent<ErrorPageProps> = (props) => {
-
+export const ErrorPage: React.FunctionComponent = (props) => {
     return (
         <ErrorBoundary
             headerTitle={ Messages.pages.error.title }
@@ -18,5 +14,3 @@ export const ErrorPageInternal: React.FunctionComponent<ErrorPageProps> = (props
         </ErrorBoundary>
     );
 };
-
-export const ErrorPage = withRouter(ErrorPageInternal);
