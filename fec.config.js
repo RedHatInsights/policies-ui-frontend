e205204b47ad0fd297e2262d9a807422e949562f
Adonis Puente <adonis.puente@gmail.com>
2025-01-30 13:07:11 -0600

    feat(RHINENG-15385): Onboard Policies to Sentry Instance

--- fec.config.js
+++ fec.config.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const packageJson = require('./package.json');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 module.exports = {
     appUrl: '/insights/policies',
@@ -9,6 +10,27 @@ module.exports = {
     interceptChromeConfig: false,
     plugins: [],
     hotReload: process.env.HOT === 'true',
+    devtool: 'hidden-source-map',
+    plugins: [
+        // Put the Sentry Webpack plugin after all other plugins
+        ...(process.env.ENABLE_SENTRY
+            ? [
+                sentryWebpackPlugin({
+                    ...(process.env.SENTRY_AUTH_TOKEN && {
+                        authToken: process.env.SENTRY_AUTH_TOKEN
+                    }),
+                    org: 'red-hat-it',
+                    project: 'policies-rhel',
+                    moduleMetadata: ({ release }) => ({
+                        dsn: `https://a9410934c7cf8b0a63576ded76dd6707@o490301.ingest.us.sentry.io/4508683264262144`,
+                        org: 'red-hat-it',
+                        project: 'policies-rhel',
+                        release
+                    })
+                })
+            ]
+            : [])
+    ],
     moduleFederation: {
         shared: [
             {
